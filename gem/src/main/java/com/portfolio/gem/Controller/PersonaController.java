c20d88ad674fbcf02c429c413fca501f8ae1449a
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-12-19 17:16:49 -0800

    Empezando a conectar el back con el front, solo con localhost

--- gem/src/main/java/com/portfolio/gem/Controller/PersonaController.java
+++ gem/src/main/java/com/portfolio/gem/Controller/PersonaController.java
@@ -9,6 +9,7 @@ import com.portfolio.gem.Entity.Persona;
 import com.portfolio.gem.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,11 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ezegi
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
 //    Ejecuta el metodo en la url
-//    Ej: https://localhost/personas/traer
+//    Ej: http://localhost/personas/traer
     @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
@@ -59,5 +61,10 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
