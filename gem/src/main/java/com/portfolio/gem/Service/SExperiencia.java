5ed2ac2908a2b2395489d4ea3cb4f5880f6e323d
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-12-21 14:24:47 -0800

    Experiencia backend

---
+++ gem/src/main/java/com/portfolio/gem/Service/SExperiencia.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.gem.Service;
+
+import com.portfolio.gem.Entity.Experiencia;
+import com.portfolio.gem.Repository.RExperiencia;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ezegi
+ */
+@Service
+@Transactional // Que aparezca tmbn en la base de datos si se hace aca
+public class SExperiencia {
+    @Autowired
+    RExperiencia rExperiencia;
+    
+    public List<Experiencia> list(){
+        return rExperiencia.findAll();
+    }
+    
+    public Optional<Experiencia> getOne(int id){
+        return rExperiencia.findById(id);
+    }
+    
+    public Optional<Experiencia> getByNombreE(String nombreE){
+        return rExperiencia.findByNombreE(nombreE);
+    }
+    
+    public void save(Experiencia expe){
+        rExperiencia.save(expe);
+    }
+    
+    public void delete(int id){
+        rExperiencia.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rExperiencia.existsById(id);
+    }
+    
+    public boolean existsByNombreE(String nombreE){
+        return rExperiencia.existsByNombreE(nombreE);
+    }
+}
